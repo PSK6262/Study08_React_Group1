e598bf8192dd919a531b517b7ad5f3d5dba15b4b
PSK6262 <qkrxotn60@gmail.com>
2026-06-23 13:59:11 +0900

    id20삭제(잘못된 정보) id1~19까지의 이미지 추가(저작권 있으므로 과제에만 사용)

--- GreenWay/Assets/components/Navigation.jsx
+++ GreenWay/Assets/components/Navigation.jsx
@@ -2,24 +2,18 @@ import { Route, Routes, Link, useNavigate } from 'react-router'
 import { Button, Spinner, Container, Nav, Navbar, Row, Col, Card } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import parksData from '../data/parksData.js'
-import Logo from '../images/logo.png'
 // 1페이지 Main Import
 import Main from './Main'
 // 2페이지 import
 import Trail from '../page2/Trail.jsx';
-
-
-import IMG from '../images/id1.png'
 function Navigation(){
-    
+    const Logo = "/images/logo.png";
     let navigate = useNavigate();
     
     return(
         <>
-
             <Navbar bg="light" data-bs-theme="light">
                 <Container>
-
                     <Navbar.Brand style={{ cursor: 'pointer' }} onClick={() => { navigate("/") }}>
                         <img src={Logo} className='logo' alt="로고"/>
                     </Navbar.Brand>
